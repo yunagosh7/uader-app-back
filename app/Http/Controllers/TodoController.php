c358916b5ba57d63bf87741434ac908ba1e26b84
yunagosh7 <yunagosh7@gmail.com>
2024-09-17 22:01:40 -0300

    Remove csrf token validation in all routes, added demo routes

--- app/Http/Controllers/TodoController.php
+++ app/Http/Controllers/TodoController.php
@@ -14,4 +14,10 @@ class TodoController extends Controller
         $todos = Todo::all();
         return $todos;
     }
+
+    public function create(Request $request) {
+        $body = $request->all();
+        Todo::create($body);
+        return $body;
+    }
 }
