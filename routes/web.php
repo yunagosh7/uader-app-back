dfe762e70a068fcdda614c9a38c89e5c4e4eab09
yunagosh7 <yunagosh7@gmail.com>
2024-10-08 19:51:30 -0300

    Added update tesis route

--- routes/web.php
+++ routes/web.php
@@ -3,13 +3,71 @@
 use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\TodoController;
+use Symfony\Component\HttpFoundation\Response;
+use App\Http\Middleware\VerifyCsrfToken;
+use Illuminate\Support\Facades\Input;
 
-$arr = array();
 
 Route::group([
     'middleware' => 'api',
-    'prefix' => 'api'
+    'prefix' => 'api',
 ], function ($router) {
+
+    // get all tesis
+    Route::get("/tesis", function(Request $request){
+        $obj = array([
+            "name" => "tesis 1",
+            "id" => 1
+        ],
+        [
+            "name" => "tesis 2",
+            "id" => 2
+        ]
+    );
+        return $obj;
+    });
+
+    Route::post("/tesis1234", function(Request $request) {
+        
+        $body = Request::all();
+        // create a record in the db
+        
+        return $body;
+    });
+
+
+    Route::post("/tesis", function(Request $request) {
+        
+        $body = Request::all();
+        // create a record in the db
+        
+        return $body;
+    });
+
+
+    Route::delete("/tesis/{tesisId}", function(Request $request){
+        $body = $request->all();
+        // $obj = array([
+        //     "name" => "tesis 1",
+        //     "id" => 1
+        // ],
+        // [
+        //     "name" => "tesis 2",
+        //     "id" => 2
+        // ]
+    // );
+        $itemId = Input::get('tesisId');
+        // get element by id
+        return $itemId;
+    });
+    Route::put("/tesis", function(Request $request) {
+        
+        $body = Request::all();
+        
+        // update a record in the db
+        
+        return $body;
+    });
 
     Route::get('/token', function () {
         return csrf_token();
@@ -26,6 +84,8 @@ Route::group([
         return "test";
         // array_push($arr, $request->);
     });
+
+    Route::post('/todos', [TodoController::class, "create"]);
 
     require __DIR__.'/auth.php';
 });
